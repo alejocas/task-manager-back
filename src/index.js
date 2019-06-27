f6f95b0c73e3634ed98326f707d2b6e7d01669c7
Alejo Castaño <alejocas98@gmail.com>
2019-06-26 23:39:13 -0500

    First steps

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const { GraphQLServer } = require('graphql-yoga');
+
+const typeDefs = `
+type Query {
+    info: String!
+}
+`
+
+const resolvers = {
+    Query: {
+        info: () => `This is the API of a Hackernews Clone`
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+});
